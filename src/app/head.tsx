2c4f0ef1ec2a07f2e3a704df980332a3c3d780ca
Bulacia <bulaciabruno@icloud.com>
2026-04-25 15:41:18 -0400

    feat: add Head component with title, description, and favicon link

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Sentinel</title>
+      <meta name="description" content="Vulnerability Scanner" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
